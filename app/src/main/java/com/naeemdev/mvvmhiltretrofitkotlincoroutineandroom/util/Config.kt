03743f4e056a4d741d897f19725d41ba9131529e
Muhammad Naeem <naeem0313@gmail.com>
2022-04-14 12:01:05 +0500

    URL Config. define themoviedb API url and Image Url

---
+++ app/src/main/java/com/naeemdev/mvvmhiltretrofitkotlincoroutineandroom/util/Config.kt
@@ -0,0 +1,7 @@
+package com.naeemdev.mvvmhiltretrofitkotlincoroutineandroom.util
+
+object Config {
+    const val BASE_URL = "https://api.themoviedb.org"
+    const val IMAGE_URL = "https://image.tmdb.org/t/p/original";
+
+}
