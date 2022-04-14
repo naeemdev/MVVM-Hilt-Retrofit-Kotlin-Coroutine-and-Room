1d734b6c8f262f7f7cda90031a7e2df635f8528b
Muhammad Naeem <naeem0313@gmail.com>
2022-04-14 12:26:22 +0500

    Movie id for get intent

--- app/src/main/java/com/naeemdev/mvvmhiltretrofitkotlincoroutineandroom/util/Config.kt
+++ app/src/main/java/com/naeemdev/mvvmhiltretrofitkotlincoroutineandroom/util/Config.kt
@@ -3,5 +3,5 @@ package com.naeemdev.mvvmhiltretrofitkotlincoroutineandroom.util
 object Config {
     const val BASE_URL = "https://api.themoviedb.org"
     const val IMAGE_URL = "https://image.tmdb.org/t/p/original";
-
+    const val MOVIE_ID = "movie_id"
 }
